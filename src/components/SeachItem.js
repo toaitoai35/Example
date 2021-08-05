d972e21628d6b336b662b6ef81848fd7c82fe4a4
Hoài Phúc <toaitoai35@gmail.com>
2021-08-05 23:44:12 +0700

    second commit

--- src/components/SeachItem.js
+++ src/components/SeachItem.js
@@ -3,47 +3,47 @@ import './style.css';
 
 function SearchItem() {
   return (
-    <div class="search">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-md-4">
-            <div class="input-group">
-              <input type="text" class="form-control" placeholder="Search item name" />
-              <div class="input-group-append">
-                <button class="btn btn-primary mb-2" type="button">Clear</button>
+    <div className="search">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="input-group">
+              <input type="text" className="form-control" placeholder="Search item name" />
+              <div className="input-group-append">
+                <button className="btn btn-primary mb-2" type="button">Clear</button>
               </div>
             </div>
           </div>
-          <div class="col-md-3">
-            <div class="form-group mb-2">
-              <div class="input-group">
-                <select name="status" class="form-control">
+          <div className="col-md-3">
+            <div className="form-group mb-2">
+              <div className="input-group">
+                <select name="status" className="form-control">
                   <option value="1">Sort by</option>
                   <option value="2">InProgress</option>
                 </select>
-                <button class="btn btn-success mb-2 name" type="button">NAME-DESC</button>
+                <button className="btn btn-success mb-2 name" type="button">NAME-DESC</button>
 
               </div>
             </div>
           </div>
-          <div class="col-md-5">
-            <button type="submit" class="btn btn-primary col-11 add">Add Item</button>
+          <div className="col-md-5">
+            <button type="submit" className="btn btn-primary col-11 add">Add Item</button>
           </div>
         </div>
       </div>
-      <div class="container-fluid">
-        <div class="row">
-        <div class="col-md-6">
+      <div className="container-fluid">
+        <div className="row">
+        <div className="col-md-6">
           </div>
-          <div class="col-md-5 sub">
-          <div class="input-group">
-          <input type="text" class="form-control col-5 s" placeholder="Item name" />
-          <select name="status" class="form-control col-2" >
+          <div className="col-md-5 sub">
+          <div className="input-group">
+          <input type="text" className="form-control col-5 s" placeholder="Item name" />
+          <select name="status" className="form-control col-2" >
               <option value="1">Small</option>
               <option value="2">Hight</option>
             </select>
-            <button class="btn btn-info mb-2 h" type="button">Submit</button>
-            <button class="btn btn-outline-secondary mb-2 h" type="button">Cancel</button>
+            <button className="btn btn-info mb-2 h" type="button">Submit</button>
+            <button className="btn btn-outline-secondary mb-2 h" type="button">Cancel</button>
             </div>
           </div>
           
--- src/components/Table.js
+++ src/components/Table.js
@@ -2,8 +2,8 @@ import './style.css';
 function Table() {
     return (
         <div className="container-fluid Table">
-            <table class="table border">
-                <thead class="table-success">
+            <table className="table border">
+                <thead className="table-success">
                     <tr>
                         <th scope="col">List Item</th>
                         <th></th>
@@ -21,43 +21,43 @@ function Table() {
                     <tr>
                         <th scope="row">1</th>
                         <td>Ở phần thi nhảy cao thuộc nội dung 10 môn phối hợp</td>
-                        <td><span class="bg-secondary text-white red">Small</span></td>
-                        <td><div class="input-group">
-                            <button class="btn btn-warning mb-2" type="button">Edit</button>
-                            <button class="btn btn-danger mb-2 h" type="button">Delete</button>
+                        <td><span className="bg-secondary text-white red">Small</span></td>
+                        <td><div className="input-group">
+                            <button className="btn btn-warning mb-2" type="button">Edit</button>
+                            <button className="btn btn-danger mb-2 h" type="button">Delete</button>
                         </div>
                         </td>
                     </tr>
                     <tr>
                         <th scope="row">2</th>
                         <td>Ở phần thi nhảy cao thuộc nội dung 10 môn phối hợp</td>
-                        <td><span class="bg-danger text-white red">High</span></td>
-                        <td><div class="input-group">
-                            <button class="btn btn-warning mb-2" type="button">Edit</button>
-                            <button class="btn btn-danger mb-2 h" type="button">Delete</button>
+                        <td><span className="bg-danger text-white red">High</span></td>
+                        <td><div className="input-group">
+                            <button className="btn btn-warning mb-2" type="button">Edit</button>
+                            <button className="btn btn-danger mb-2 h" type="button">Delete</button>
                         </div>
                         </td>
                     </tr>
                     <tr>
                         <th scope="row">3</th>
                         <td>Ở phần thi nhảy cao thuộc nội dung 10 môn phối hợp</td>
-                        <td><span class="bg-info text-white red">Medium</span></td>
-                        <td><div class="input-group">
-                            <button class="btn btn-warning mb-2" type="button">Edit</button>
-                            <button class="btn btn-danger mb-2 h" type="button">Delete</button>
+                        <td><span className="bg-info text-white red">Medium</span></td>
+                        <td><div className="input-group">
+                            <button className="btn btn-warning mb-2" type="button">Edit</button>
+                            <button className="btn btn-danger mb-2 h" type="button">Delete</button>
                         </div>
                         </td>
                     </tr>
                     <tr>
                         <th scope="row">4</th>
-                        <td> <input type="text" class="form-control" placeholder="Ở phần thi nhảy cao thuộc nội dung 10 môn phối hợp" /></td>
-                        <td><select name="status" class="form-control">
+                        <td> <input type="text" className="form-control" placeholder="Ở phần thi nhảy cao thuộc nội dung 10 môn phối hợp" /></td>
+                        <td><select name="status" className="form-control">
                             <option value="1">Small</option>
                             <option value="2"></option>
                         </select></td>
-                        <td><div class="input-group">
-                            <button class="btn btn-outline-secondary mb-2" type="button">Cancel</button>
-                            <button class="btn btn-success mb-2 h" type="button">Save</button>
+                        <td><div className="input-group">
+                            <button className="btn btn-outline-secondary mb-2" type="button">Cancel</button>
+                            <button className="btn btn-success mb-2 h" type="button">Save</button>
                         </div>
                         </td>
                     </tr>
