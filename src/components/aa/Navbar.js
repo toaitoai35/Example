5eaf055e583783d4312046a3c055315e2e71e351
Hoài Phúc <toaitoai35@gmail.com>
2021-08-20 19:18:04 +0700

    s

---
+++ src/components/aa/Navbar.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import './navbar.css';
+import { MDBDropdown,MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem} from "mdbreact";
+
+function Navbar()  {
+const [showLinks, setShowLinks] = useState(false);
+const [dropdown, setDropdown] = useState(false);
+
+  return (
+   <div className="Navbar mb-2">
+     <div className="left">
+     <i className="fas fa-bars" onClick={()=> setShowLinks(!showLinks)}></i>
+     <strong className="white-text">Navbar</strong>
+     <div className="links" id={showLinks ? "hidden" : ""}>
+     
+     <a href="/">Home</a>
+      <a href="/features">Features</a>
+      <a href="/pricing">Pricing</a> 
+    
+      <MDBDropdown>
+                <MDBDropdownToggle nav caret>
+                  Dropdown
+                </MDBDropdownToggle>
+                <MDBDropdownMenu className="dropdown-default">  
+                  <MDBDropdownItem href="#!">Action</MDBDropdownItem>
+                  <MDBDropdownItem href="#!">Another Action</MDBDropdownItem>
+                  <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+                  <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+                </MDBDropdownMenu>
+              </MDBDropdown>
+      
+     </div>
+     </div>
+     <div className="right">
+     <div className="icon">
+     <i className="fab fa-twitter"></i>
+     <i className="fab fa-google-plus-g"></i>
+     <i className="fas fa-user-alt"></i>
+     </div>
+     </div>
+  
+   </div>
+   
+    );
+  }
+
+export default Navbar;
